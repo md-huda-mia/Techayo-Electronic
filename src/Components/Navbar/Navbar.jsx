472a63184b570759b5faad5e425d0548665d511f
md-huda-mia <mdhudamia.dev@gmail.com>
2023-12-06 22:06:58 +0600

    navabr

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,12 @@ import "./Navbar.css";
 import { MdLocalShipping } from "react-icons/md";
 import { IoMdSearch } from "react-icons/io";
 import { MdOutlineLogin } from "react-icons/md";
-
+import { RiLogoutBoxLine } from "react-icons/ri";
+import { CiUser } from "react-icons/ci";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   return (
     <>
       <div className="header">
@@ -26,13 +30,88 @@ const Navbar = () => {
               <IoMdSearch />
             </button>
           </div>
-          <div className="user">
-            <div className="icon">
-              <MdOutlineLogin />
+
+          {isAuthenticated ? (
+            <div className="user">
+              <div className="icon">
+                <RiLogoutBoxLine />
+              </div>
+              <div className="btn">
+                <button
+                  onClick={() =>
+                    logout({
+                      logoutParams: { returnTo: window.location.origin },
+                    })
+                  }>
+                  LogOut
+                </button>
+              </div>
             </div>
-            <div className="btn">
-              <button>Login</button>
+          ) : (
+            <div className="user">
+              <div className="icon">
+                <MdOutlineLogin />
+              </div>
+              <div className="btn">
+                <button onClick={() => loginWithRedirect()}>Login</button>
+              </div>
             </div>
+          )}
+        </div>
+        <div className="last_header">
+          <div className="user_profile">
+            {isAuthenticated ? (
+              <>
+                <div className="icon">
+                  <CiUser />
+                </div>
+                <div className="info">
+                  <h2>{user.name}</h2>
+                  <p>{user.email}</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="icon">
+                  <CiUser />
+                </div>
+                <div className="info">
+                  <p>Please login</p>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="nav">
+            <ul>
+              <li>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/shop" className="link">
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link to="/collection" className="link">
+                  Collection
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="offer">
+            <p>flat 10% over all iphone</p>
           </div>
         </div>
       </div>
